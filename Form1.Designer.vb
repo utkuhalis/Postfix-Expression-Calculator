904b785ae25a25d5b87c8063f1736a1bc983b12c
Utku Halis <personal@utkuhalis.com>
2022-01-15 04:50:55 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(493, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Expression"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(433, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Solve"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(514, 8)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(274, 394)
+        Me.ListBox1.TabIndex = 3
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(100, 55)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(97, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Manual Control"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 400)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Next"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(15, 78)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(493, 316)
+        Me.ListBox2.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(325, 51)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(102, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Sample Data"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(15, 55)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "Slow Mode"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not TextBox1.Text Is "" Then
+            ListBox1.Items.Clear()
+            ListBox2.Items.Clear()
+
+            For Each character As String In TextBox1.Text
+                ListBox1.Items.Add(character)
+            Next
+
+            ListBox1.SelectedIndex = 0
+
+            If CheckBox1.Checked = False Then
+                Button2.Enabled = True
+
+                While ListBox1.Items.Count > 1
+                    Button2.PerformClick()
+                    Me.Refresh()
+                    If CheckBox2.Checked Then
+                        Threading.Thread.Sleep(500)
+                    End If
+
+                    If ListBox2.Items.Contains("Expression Syntax Error!") Then
+                        Exit While
+                    End If
+
+                End While
+
+                Button2.Enabled = False
+            End If
+        Else
+            ListBox2.Items.Add("Expression Syntax Error!")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim beforeN = 0
+            Dim beforeN2 = 0
+            Dim Result = 0
+            Dim exp = ""
+
+            If ListBox1.SelectedItem.ToString = "-" Then
+                exp = "-"
+            ElseIf ListBox1.SelectedItem.ToString = "+" Then
+                exp = "+"
+            ElseIf ListBox1.SelectedItem.ToString = "*" Then
+                exp = "*"
+            ElseIf ListBox1.SelectedItem.ToString = "$" Then
+                exp = "$"
+            End If
+
+            If Not exp Is "" Then
+                beforeN = Convert.ToInt32(ListBox1.Items.Item(ListBox1.SelectedIndex - 2))
+                beforeN2 = Convert.ToInt32(ListBox1.Items.Item(ListBox1.SelectedIndex - 1))
+
+                If exp = "-" Then
+                    Result = beforeN - beforeN2
+                ElseIf exp = "+" Then
+                    Result = beforeN + beforeN2
+                ElseIf exp = "*" Then
+                    Result = beforeN * beforeN2
+                ElseIf exp = "$" Then
+                    Result = beforeN ^ beforeN2
+                End If
+
+                ListBox2.Items.Add(beforeN & " - " & beforeN2 & " = " & Result)
+                ListBox1.Items.Insert(ListBox1.SelectedIndex - 2, Result)
+                ListBox1.Items.RemoveAt(ListBox1.SelectedIndex - 2)
+                ListBox1.Items.RemoveAt(ListBox1.SelectedIndex - 1)
+                ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+            End If
+
+            If ListBox1.SelectedIndex < ListBox1.Items.Count - 1 Then
+                ListBox1.SelectedIndex += 1
+            End If
+        Catch ex As Exception
+            ListBox2.Items.Add("Expression Syntax Error!")
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = "51-325+**2+5-"
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Button2.Enabled = CheckBox1.Checked
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.Text = Application.ProductName
+        Me.MaximizeBox = False
+        Me.DoubleBuffered = True
+        Me.MaximumSize = New Size(Me.Width, Me.Height)
+        Me.MinimumSize = New Size(Me.Width, Me.Height)
+        Button2.Enabled = False
+    End Sub
+End Class
